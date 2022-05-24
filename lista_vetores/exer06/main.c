34b5a86f616d3c9930d5db1189a84c6f90d283fe
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-05-24 16:58:49 -0300

    att

---
+++ lista_vetores/exer06/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char string[30];
+
+    printf("Seu nome\n");
+    scanf("%[^\n]",string);
+
+    for (int i = strlen(string); i >= 0; i--)
+    {
+        
+             printf("%c",string);
+          
+    }
+
+
+    return 0;
+}
