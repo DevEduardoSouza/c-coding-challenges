cce309638d92e4fd77eda6c7933cabdc0d19480d
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-05-24 16:21:46 -0300

    att

--- lista_vetores/exer02/main.c
+++ lista_vetores/exer02/main.c
@@ -1,7 +1,26 @@
+/*Escreva um programa em C para contar o número total de palavras em uma string.*/
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
+    int  cont = 0;
+    char string[60];
+
+    printf("Escreva para eu descobir total de palavras");
+    scanf("%[^\n]",string[60]);
+
+    for (int i = 0; i <= 61; i++)
+    {
+        if (string == ' ');
+        {
+            cont++;
+        }
+        
+    }
+
+    printf("Total de palavavras e : %d", cont);
+    
+
    
     return 0;
 }
