14e0c56a087b24c5c0b1404b22570b9e054574b8
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-05-27 12:05:55 -0300

    att

--- lista_vetores/exer11/main.c
+++ lista_vetores/exer11/main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
    
-   int vetor[5],posi=0;
+  int vetor[6], par[6]={0}, impar[6] = {0},contPar=0,contImpar= 0 ;
 
-   for (int i = 1; i < 6; i++)
-   {
-       printf("Informe o %i° valor: ",i);
-       scanf("%i",&vetor[i]);
-   }
-   printf("Qual posicao no vetor voce que saber: ");
-   scanf("%i",&posi);
+   printf("Informe 5 valores: ");
+  for (int i = 0; i < 5; i++)
+  {
+      scanf("%d",&vetor[i]);
+  }
 
-   for (int i = 1; i < 6; i++)
-   {
-       if (posi == vetor[i])
-       {
-        printf("%i na posicao %i", posi,i);
-       } 
-   }
-   
+  for (int i = 0; i < 5; i++)
+  {
+      if (vetor[i] % 2 == 0)
+      {
+          par[i] = vetor[i];
+          ++contPar;
+      }
+      if(vetor[i] % 2 == 1){
+          impar[i] = vetor[i];
+          ++contImpar;
+      }
+  }
+
+
+  printf("Os elemetos pares sao: \n");
+  for (int i = 0; i <= contPar; i++)
+  {
+      if (par[i] != 0)
+      {
+          printf(" %d ",par[i]);
+      }
+  }
+
+printf(" \n\n ");
+
+printf("Os elemetos impares sao: \n");
+  for (int i = 0; i <= contImpar; i++)
+  {
+      if (impar[i] != 0)
+      {
+          printf(" %d ",impar[i]);
+      }
+      
+     
+  }
+  
+  
+  
     return 0;
 }
