1331c6c94cbc1e7f23bfd3afcf85e87ba70e096a
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-05-28 21:31:56 -0300

    att

---
+++ lista_vetores/exer10/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    char nome[30];
+
+    printf("Informe seu nome: ");
+    gets(nome);
+
+    for (int i = 0; i < 30; i++)
+    {
+        if (nome[i] == strupr)
+        {
+            printf("IHHEEEEE!!!");
+        }
+        
+    }
+    
+    
+    return 0;
+}
