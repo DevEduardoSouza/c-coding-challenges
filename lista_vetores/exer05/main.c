1effa43be01a92424554477b015a6b5bb4a2462a
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-06-02 13:25:06 -0300

    att

--- lista_vetores/exer05/main.c
+++ lista_vetores/exer05/main.c
@@ -1,65 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-    int num[10] = {0};
-    int novoValor, posicao, aux, cont=1, aux2;
-
-    
-    for (int i = 0; i < 4; i++)
+    int vetor_ordenado[10]={0};
+    int posicao=0, cont_qtd_valores = 0, valor;
+    do
     {
-        printf("Informe um valor: ");
-        scanf("%d",&num[i]);
-
-        if (num[i] == -1)
+        printf("\nDigite um valor: ");
+        scanf("%d",&valor);
+        posicao = 0;
+        while (vetor_ordenado[posicao] < valor && vetor_ordenado[posicao]!=0)
         {
-            break;
-        }
-        
-    }
-
-    printf("Lista atual de valores no vetor\n");
-    for (int i = 0; i < 4; i++)
-    {
-        printf(" %d",num[i]);
-    }
-
-
-    printf("\nInsira um novo valor: ");
-    scanf("%d",&novoValor); 
-    printf("\nInsira a posicao onde o novo valor a ser inserido: ");
-    scanf("%d",&posicao); 
-
-    for (int i = 0; i < 4; i++)
-    {
-        if (posicao == i)
-        {
-               aux = num[i];
-                num[i] = novoValor;
-            
-            for (int j = 1; j < 5; j++)
-            {
-                aux = num[i+j];
-                num[i+j] = novoValor;
- 
-            }
-            
+            posicao++;
         }
 
+        for (int j = cont_qtd_valores; j >= posicao; j--)
+        {
+            vetor_ordenado [j+1] = vetor_ordenado[j];
+        }
+        cont_qtd_valores++;
+        vetor_ordenado[posicao] = valor;
+
+        for (int i = 0; i < cont_qtd_valores; i++)
+        {
+            printf("%d, ", vetor_ordenado[i]);
+        }
         
-    }
-
-    
-    printf("\nApos inserir o elemento, a nova lista e: ");
-    for (int i = 0; i < 4; i++)
-    {
-        printf("\n%d",num[i]);
-    }
-    
+        
+    } while (valor !=-1 && cont_qtd_valores <10);
     
 
-    
     return 0;
 }
+
