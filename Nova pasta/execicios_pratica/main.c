33c8eb6ba41a86720f1e9bad71fc12358f297966
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-06-03 16:55:58 -0300

    lista

--- Nova pasta/execicios_pratica/main.c
+++ Nova pasta/execicios_pratica/main.c
@@ -1,5 +1,11 @@
+/*Link:
+
+    https://classroom.google.com/u/0/w/NDU5Njg3NTU3MDE2/t/all
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 
 int exer001()
@@ -154,8 +160,144 @@ int exer005()
        printf("%d, ",par[i]);
    }
 }
+int exer006()
+{
+    /*
+    Fac ̧a um programa que receba do usuario um vetor com 10 posic ̧  ́ oes. Em seguida dever  ̃ a ́
+     ser impresso o maior e o menor elemento do vetor.
+            
+    */
+
+   int vetor[10], maiorNumero=0, menorNumero=0;
+  
+
+   for (int i = 0; i < 10; i++)
+   {
+       printf("Digite um valor: ");
+       scanf("%d",&vetor[i]);
+
+       if (i == 0)
+       {
+           menorNumero = vetor[i];
+       }
+       
+       if (vetor[i] > maiorNumero)
+       {
+          maiorNumero = vetor[i];
+       }
+       if (vetor[i] < menorNumero)
+       {
+           menorNumero = vetor[i];
+       }
+   }
+
+   printf("\n\nO maior numero: %d\n",maiorNumero);
+   printf("O menor numero: %d\n",menorNumero);
+}
+int exer007()
+{
+    /*
+   Escreva um programa que leia 10 numeros inteiros e os armazene em um vetor. Imprima o vetor, o maior elemento e a posic ̧ao que ele se encontra.
+    */
+
+   int vetor[10], maiorNumero=0, posicaoMaior;
+
+   for (int i = 1; i < 11; i++)
+   {
+       printf("Digite um valor inteiro: ");
+       scanf("%d",&vetor[i]);
+   }
+
+   for (int i = 1; i < 11; i++)
+   {
+        if (vetor[i] > maiorNumero)
+        {
+            maiorNumero = vetor[i];
+            posicaoMaior = i;
+        }
+   }
+   
+
+   printf("\n\nO numero maior e %d e esta na posicao %d",maiorNumero,posicaoMaior);
+}
+int exer008()
+{
+    /*
+   Crie um programa que le 6 valores inteiros e, em seguida, mostre na tela os valores lidos na ordem inversa.
+    */
+
+   int vetor[6];
+
+   for (int i = 0; i < 6; i++)
+   {
+       printf("Digite um valor inteiro: ");
+       scanf("%d",&vetor[i]);
+   }
+
+   printf("\n\nos numero em ordem inversa: ");
+   for ( int i = 5; i >= 0; i--)
+   {
+     printf(" %d",vetor[i]);
+   }
+   
+}
+int exer009()
+{
+    /*
+    Crie um programa que le 6 valores inteiros pares e, em seguida, mostre na tela os valores lidos na ordem inversa.
+    */
+
+   int vetor[6], par[6], j=0, contaPar=0;
+
+   for (int i = 0; i < 6; i++)
+   {
+       printf("Digite um valor inteiro : ");
+       scanf("%d",&vetor[i]);
+
+       if (vetor[i] % 2 == 0)
+       {
+           par[j] = vetor[i]; 
+           j++;
+           contaPar++;
+       }
+       
+   }
+
+   printf("\n\nos numero em ordem inversa dos numeros pares: ");
+   for ( int i = contaPar - 1; i >= 0; i--)
+   {
+     printf(" %d",par[i]);
+   }
+   
+}
+int exer010()
+{
+    /*
+    Fac ̧a um programa para ler a nota da prova de 15 alunos e armazene num vetor, calcule e imprima a media geral.
+    */
+
+   float nota[15], mediaTurma, somaNotas=0;
+
+   for (int i = 1; i < 16; i++)
+   {
+       printf("Insira a nota do  aluno %d : ",i);
+       scanf("%f",&nota[i]); 
+   }
+   
+   for (int i = 1; i < 16; i++)
+   {
+       somaNotas = somaNotas + nota[i];
+   }
+
+   mediaTurma =  somaNotas / 15.0;
+   
+   system("cls");
+   printf("\n\nA medida da turma %.2f\n\n",mediaTurma);
+   
+}
+
 int main()
 {
-    exer005();
+    exer010();
     return 0;
 }
